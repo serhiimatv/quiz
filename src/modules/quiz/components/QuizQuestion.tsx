c1222617959f49431252307b0222420a83f9d89e
Сергей Матвиевский <crm.33739@dz.com>
2024-10-29 23:18:25 +0200

    fix

--- src/modules/quiz/components/QuizQuestion.tsx
+++ src/modules/quiz/components/QuizQuestion.tsx
@@ -57,62 +57,60 @@ const QuizQuestion: FC<IProp> = ({
   };
 
   return (
-    <>
-      <div className="flex items-center justify-center mt-12">
-        <div className="relative flex flex-col w-full max-w-[48rem] rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-          <div className="p-6">
-            <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-              {question}
-            </h4>
-          </div>
-          <div className="p-6 mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-            <fieldset>
-              {type === "boolean" || type === "single"
-                ? choices.map((item) => {
-                    return (
-                      <div key={v4()} className="mt-2">
-                        <input
-                          type="radio"
-                          id={item}
-                          name={item}
-                          onChange={singleChoiceHandler}
-                          checked={single === item}
-                        />
-                        <label className="ml-2" htmlFor={item}>
-                          {item}
-                        </label>
-                      </div>
-                    );
-                  })
-                : choices.map((item) => {
-                    return (
-                      <div className="mt-2" key={v4()}>
-                        <input
-                          type="checkbox"
-                          id={item}
-                          name={item}
-                          onChange={multipleChoiceHandler}
-                          checked={Boolean(
-                            multiple.find((answer) => answer === item)
-                          )}
-                        />
-                        <label className="ml-2" htmlFor={item}>
-                          {item}
-                        </label>
-                      </div>
-                    );
-                  })}
-              <Button
-                title="Вибрати"
-                color="purple"
-                className="mt-2"
-                onClick={selectAnswerHandler}
-              />
-            </fieldset>
-          </div>
+    <div className="flex items-center justify-center mt-12">
+      <div className="relative flex flex-col w-full max-w-[48rem] rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className="p-6">
+          <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            {question}
+          </h4>
+        </div>
+        <div className="p-6 mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+          <fieldset>
+            {type === "boolean" || type === "single"
+              ? choices.map((item) => {
+                  return (
+                    <div key={v4()} className="mt-2">
+                      <input
+                        type="radio"
+                        id={item}
+                        name={item}
+                        onChange={singleChoiceHandler}
+                        checked={single === item}
+                      />
+                      <label className="ml-2" htmlFor={item}>
+                        {item}
+                      </label>
+                    </div>
+                  );
+                })
+              : choices.map((item) => {
+                  return (
+                    <div className="mt-2" key={v4()}>
+                      <input
+                        type="checkbox"
+                        id={item}
+                        name={item}
+                        onChange={multipleChoiceHandler}
+                        checked={Boolean(
+                          multiple.find((answer) => answer === item)
+                        )}
+                      />
+                      <label className="ml-2" htmlFor={item}>
+                        {item}
+                      </label>
+                    </div>
+                  );
+                })}
+            <Button
+              title="Вибрати"
+              color="purple"
+              className="mt-2"
+              onClick={selectAnswerHandler}
+            />
+          </fieldset>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
